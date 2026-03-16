5a0ba2effe7b03561934e2d8568f7178cee81070
madlena-turcan <madlena.turcan@bearingpoint.com>
2026-03-16 10:43:50 +0200

    Refactor project navigation and update file permissions

--- script.js
+++ script.js
@@ -1,69 +1,57 @@
-$(document).ready(()=>{
-    $('#universal').hide();
-    $('#scarlet').hide();
-    $('#cuget').hide();
-    $('#osf-click').addClass('clickWork');
-    $('#osf-click').on('click', (event)=>{
-        $(event.currentTarget).addClass('clickWork');
-        $('#osf').show();
-        $('#universal').hide();
-        $('#scarlet').hide();
-        $('#cuget').hide();
-        $('#scarlet-click').removeClass('clickWork');
-        $('#cuget-click').removeClass('clickWork');
-        $('#universal-click').removeClass('clickWork');
-    });
-    $('#universal-click').on('click', (event)=>{
-        $(event.currentTarget).addClass('clickWork');
-        $('#universal').show();
-        $('#osf').hide();
-        $('#scarlet').hide();
-        $('#cuget').hide();
-        $('#scarlet-click').removeClass('clickWork');
-        $('#cuget-click').removeClass('clickWork');
-        $('#osf-click').removeClass('clickWork');
-    });
-    $('#scarlet-click').on('click', (event)=>{
-        $(event.currentTarget).addClass('clickWork');
-        $('#scarlet').show();
-        $('#osf').hide();
-        $('#universal').hide();
-        $('#cuget').hide();
-        $('#osf-click').removeClass('clickWork');
-        $('#cuget-click').removeClass('clickWork');
-        $('#universal-click').removeClass('clickWork');
-    });
-    $('#cuget-click').on('click', (event)=>{
-        $(event.currentTarget).addClass('clickWork');
-        $('#cuget').show();
-        $('#osf').hide();
-        $('#scarlet').hide();
-        $('#universal').hide();
-        $('#scarlet-click').removeClass('clickWork');
-        $('#osf-click').removeClass('clickWork');
-        $('#universal-click').removeClass('clickWork');
-    });
+$(document).ready(() => {
+  // Hide all projects except the first one initially
+  $("#project2").hide();
+  $("#project3").hide();
+  $("#project1-click").addClass("clickWork");
 
-    // Mobile navigation
+  // Project 1 click handler
+  $("#project1-click").on("click", (event) => {
+    $(event.currentTarget).addClass("clickWork");
+    $("#project1").show();
+    $("#project2").hide();
+    $("#project3").hide();
+    $("#project2-click").removeClass("clickWork");
+    $("#project3-click").removeClass("clickWork");
+  });
 
-    $('#x-icon').hide();
-    $('#burger-icon').on('click', ()=>{
-        $('.nav-burger-menu-cnt').show();
-        $('#burger-icon').hide();
-        $('#x-icon').show();
-        $('#image-logo').hide();
-    });
-    $('#x-icon').on('click', ()=>{
-        $('.nav-burger-menu-cnt').hide();
-        $('#burger-icon').show();
-        $('#x-icon').hide();
-        $('#image-logo').show();
-    });
-    $('.nav-item-burger').on('click',()=>{
-        $('.nav-burger-menu-cnt').hide();
-        $('#burger-icon').show();
-        $('#x-icon').hide();
-        $('#image-logo').show();
-    })
+  // Project 2 click handler
+  $("#project2-click").on("click", (event) => {
+    $(event.currentTarget).addClass("clickWork");
+    $("#project2").show();
+    $("#project1").hide();
+    $("#project3").hide();
+    $("#project1-click").removeClass("clickWork");
+    $("#project3-click").removeClass("clickWork");
+  });
 
+  // Project 3 click handler
+  $("#project3-click").on("click", (event) => {
+    $(event.currentTarget).addClass("clickWork");
+    $("#project3").show();
+    $("#project1").hide();
+    $("#project2").hide();
+    $("#project1-click").removeClass("clickWork");
+    $("#project2-click").removeClass("clickWork");
+  });
+
+  // Mobile navigation
+  $("#x-icon").hide();
+  $("#burger-icon").on("click", () => {
+    $(".nav-burger-menu-cnt").show();
+    $("#burger-icon").hide();
+    $("#x-icon").show();
+    $("#image-logo").hide();
+  });
+  $("#x-icon").on("click", () => {
+    $(".nav-burger-menu-cnt").hide();
+    $("#burger-icon").show();
+    $("#x-icon").hide();
+    $("#image-logo").show();
+  });
+  $(".nav-item-burger").on("click", () => {
+    $(".nav-burger-menu-cnt").hide();
+    $("#burger-icon").show();
+    $("#x-icon").hide();
+    $("#image-logo").show();
+  });
 });
